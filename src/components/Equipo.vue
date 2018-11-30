afd98f1ad71d0873568b2b77152e1273216e07e1
yesid <yesison714@gmail.com>
2018-11-29 19:46:05 -0500

    Adición del componente Equipo

---
+++ src/components/Equipo.vue
@@ -0,0 +1,42 @@
+<template>
+    <b-card :header="nombre" :img-src="Logo" img-alt="Equipos"
+    style="max-width:20rem;"
+    class="mb-2"
+    >
+
+    <p class="card-text">
+        Estado: <div :class="{'activo':estado,'inactivo':!estado}" 
+        class="circulo"></div><br/>
+        Jugadores: {{jugadores.length}}
+    </p>
+    <b-button href="#" variant="primary">Ver jugadores</b-button>
+    </b-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            nombre: "Equipo",
+            Logo: "https://via.placeholder.com/600x300/D2B48C/ffffff?text=Logo+equipo",
+            estado:true,
+            jugadores:[{jugador:"1"},{jugador:"1"}, {jugador:"1"}]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.circulo{
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    margin: 0 auto;
+}
+.activo{
+    background-color: green;
+}
+.inactivo{
+    background-color: red;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    
+   <Equipo/>
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Equipo from 
+"@/components/Equipo.vue";
+
+export default {
+  name: 'home',
+  components: {
+    Equipo
+  }
+}
+</script>
